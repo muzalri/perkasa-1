eaf91be59662db3cc77bccda4877382981d0de74
muzalri <mzakialgifari5647@gmail.com>
2024-12-18 00:57:06 +0700

    Update index.vue

--- pages/komunitas/index.vue
+++ pages/komunitas/index.vue
@@ -17,7 +17,7 @@
         <button 
           @click="selectedCategory = null"
           :class="[
-            'inline-block w-3 py-2 rounded-full transition-all duration-300 transform hover:scale-105 flex items-center justify-center',
+            'inline-block w-32 py-2 rounded-full transition-all duration-300 transform hover:scale-105 flex items-center justify-center',
             !selectedCategory ? 'bg-teal-600 text-white h-10' : 'bg-gray-100 text-gray-700 hover:bg-gray-200 h-10'
           ]"
         >
@@ -28,7 +28,7 @@
           :key="category.id"
           @click="selectedCategory = category.id"
           :class="[
-            'inline-block w-3 py-2 rounded-full transition-all duration-300 transform hover:scale-105 flex items-center justify-center',
+            'inline-block w-32 py-2 rounded-full transition-all duration-300 transform hover:scale-105 flex items-center justify-center',
             selectedCategory === category.id ? 'bg-teal-600 text-white h-10' : 'bg-gray-100 text-gray-700 hover:bg-gray-200 h-10'
           ]"
         >
